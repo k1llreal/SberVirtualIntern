96f7ba0e486bf012567785a9ea3c240278ba987d
Kirill Golovan <ki.golovan@yandex.ru>
2023-02-22 22:05:41 +0300

    Second task commit

---
+++ src/main/java/task2/Main.java
@@ -0,0 +1,8 @@
+package task2;
+
+public class Main {
+    public static void main(String[] args) {
+        CityUtils.printFirstSort(CityUtils.parse()); // 1 сортировка
+        CityUtils.printSecondSort(CityUtils.parse()); // 2 сортировка
+    }
+}
